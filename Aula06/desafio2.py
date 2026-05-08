d2bbb8c4d8d20861caf94121291e53e56be15e6b
garotodeprograma <cordeirofelipe007@gmail.com>
2026-05-07 21:59:07 -0300

    Update desafio2.py

--- Aula06/desafio2.py
+++ Aula06/desafio2.py
@@ -18,26 +18,24 @@ while True:
     if 0<= nota <=10:
         lista_notas.append(nota)
         print(lista_notas)
-        status = 0
+     
     else:
         print("Nota Inválida")
-        status = 1
+    
         break
     opcao = input("Deseja adicionar mais notas?(Enter - continua | n - Não)").lower()
 
     if opcao == "n":
         break
 
-if status <= 0:
-   
-    media = sum(lista_notas) / len(lista_notas)
 
-    if media >= 7:
-        print("Aprovado")
-    elif media >= 5:
-        print("Recuperação")
-    else:
-        print("Reprovado")
-else:
-    print("Impossivel calcular a média")
    
+media = sum(lista_notas) / len(lista_notas)
+print("sua média final foi: ",media)
+if media >= 7:
+        print("Aprovado")
+elif media >= 5:
+        print("Recuperação")
+else:
+        print("Reprovado")
+    
